7dc1be77a252986a46d3882eac8a0d5dae4c46e2
Dinesh Ghutake <dinesh.ghutake@tieto.com>
2021-10-27 08:45:11 +0530

    initial commit for static website, elastic beanstalk and EFS

---
+++ brainupgrade-aws-springboot-eb/src/main/java/com/brainupgrade/aws/eb/BrainupgradeAwsSpringbootEbApplication.java
@@ -0,0 +1,13 @@
+package com.brainupgrade.aws.eb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainupgradeAwsSpringbootEbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainupgradeAwsSpringbootEbApplication.class, args);
+	}
+
+}
